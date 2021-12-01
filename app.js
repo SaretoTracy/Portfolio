17f860d4e28e82f790bb21e4bbca8bfdb9d30415
saretotracy <tracysareto@gmail.com>
2021-12-01 22:51:35 +0300

    loop span element through the array

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const txtLogo = text.textContent;
 const splittxtLogo = txtLogo.split("");
 
 console.log(splittxtLogo);
+text.textContent = ("");
+
+for (let i = 0; i < splittxtLogo.length; i++) {
+    text.innerHTML += "<span>" + splittxtLogo[i] + "</span>"
+}
