92bd2d0eb3ff682661741d5bdcb87096f78a96ae
saretotracy <tracysareto@gmail.com>
2021-12-04 23:51:00 +0300

    socials js

--- socials.js
+++ socials.js
@@ -1,7 +1,7 @@
-fetch('navbar.html')
+fetch('socials.html')
     .then(res => res.text())
     .then(text => {
-        let oldelem = document.querySelector("script#replace_with_navbar");
+        let oldelem = document.querySelector("script#replace_with_socials");
         let newelem = document.createElement("div");
         newelem.innerHTML = text;
         oldelem.parentNode.replaceChild(newelem, oldelem);
