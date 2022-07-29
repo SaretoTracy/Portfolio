119ddaca20b55cceb35c20fbd65ce6db74a530c4
Tracy Sareto <tracysareto@gmail.com>
2022-07-30 02:24:43 +0300

    create and style skills page

--- port.js
+++ port.js
@@ -1,6 +1,6 @@
 gsap.to(".header", {duration: 2, x:50});
-gsap.to(".name", {duration: 3, x:200});
-gsap.to(".intro", {duration: 3, x:-100});
+gsap.to(".name", {duration: 2, x:200});
+gsap.to(".intro", {duration: 2, x:-100});
 
 const countAll=document.getElementsByClassName(".port")
 function content(){
